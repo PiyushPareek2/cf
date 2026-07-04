985d052f3eb561a99d9b592ac3089216814ee67c
PiyushPareek2 <piyush85299@gmail.com>
2026-07-04 15:09:12 +0530

    daily coding

---
+++ C_Kuroni_and_Impossible_Calculation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+   int n,m;
+   cin>>n>>m;
+   vector<int> a(n);
+   for(auto&x : a)cin>>x;
+   if(n>=m){
+    cout<<0<<endl;
+    return 0;
+   }
+   int ans=1;
+   for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        ans=(ans*abs(a[i]-a[j]))%m;
+    }
+   }
+   cout<<ans<<endl;
+    
+    return 0;
+}
